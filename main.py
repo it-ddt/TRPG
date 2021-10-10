cf334360adacbae0b45bae5db6143b24019ddd46
it-ddt <45958272+it-ddt@users.noreply.github.com>
2021-10-10 19:28:02 +0300

    dice fixes
    Модуль Dice теперь возвращает int вместо None
    Сделаны докстроки для Main и Dice
    Добавлены комментарии в трудных местах

--- main.py
+++ main.py
@@ -1,25 +1,60 @@
-import dice
+import os  # для очистки экрана
+import dice  # для игры в кости
 
 
-def show_location_home():
-	user_money = 5000  # деньги не должны переопределяться с каждым вызовом этой функции
+def show_location_home(user_money: int):
+    """
+        Принимает деньги пользователя.
+        Очищает экран и печатает варианты действий.
+        Дает выбирать из этих действий.
+        Игра завершается, если переменная game тановится False.
+        
+        TODO:
+            Передать сюда все параметры пользователя.
+            Печатать параметры пользователя.
+            Пробросить user_luck в игру в кости.
+            Использовать user_name место Ты/Вы.
+            Использовать переменную game для выхода из игры.
 
-	# пишем, что можно тут делать
-	print("1 - ничего не делать")
-	print("2 - сыграть в кости с разбойниками")
-	
-	# выбираем вариант
-	user_choice = "0"
-	while user_choice not in ("1", "2"):
-		user_choice = input("Что делать? ")
+            Подключить модули Арены и Магазина.
+    """
 
-	if user_choice == "1":
-		print("Z-z-z")
-	elif user_choice == "2":
-		user_money = dice.play_dice(user_money)
-		print("У тебя теперь", user_money)
-	else:
-		print("Что-то пошло не так")
+    # Очищаем экран и описываем возможные варианты.
+    os.system("cls")
+    print("Ты дома.")
+    print("1 - ничего не делать")
+    print("2 - сыграть в кости с разбойниками")
+    print("3 - выйти из игры")
+
+    # Выбираем вариант, пока он не будет одним из возможных.
+    user_choice = "0"
+    while user_choice not in ("1", "2", "3"):
+        user_choice = input("Что делать? ")
+
+    # Действуем по выбранному пользователем варианту.
+    if user_choice == "1":
+        print("Z-z-z")
+    elif user_choice == "2":
+        user_money = dice.play_dice(user_money)
+    elif user_choice == "3":
+        print("Выходим из игры")
+    else:
+        print("Что-то пошло не так")
+
+    # Чтобы экран не очистился раньше, чем пользователь увидит
+    # результат своих действий.
+    input("Нажми ENTER для продолжения")
 
 
-show_location_home()
+# создаем персонажа
+user_name = "Вася Питонов"
+user_money = 5000
+user_luck = 30
+user_hp = 100
+game = True
+
+# начинаем играть
+show_location_home(user_money)
+
+print("Конец. Спасибо за игру!")
+input("Нажми ENTER для выхода")
